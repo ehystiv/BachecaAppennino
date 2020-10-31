43c8c691268e6f102ffa2379286b6093f406328f
StefanoBichicchi <bichicchi.stefano@gmail.com>
2020-10-31 11:40:02 +0100

    token validato middleware

---
+++ api/validateToken.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken')
+
+module.exports = function (req, res, next) {
+  req.JWT = {
+    token: req?.headers?.authorization.split(' ')[1],
+    isValid() {
+      try {
+        jwt.verify(this.token, process.env.JWT_SECRET)
+        return true
+      } catch (e) {
+        console.warn('Bad jwt token')
+        return false
+      }
+    },
+    getData() {
+      return jwt.decode(this.token)
+    },
+  }
+
+  next()
+}
