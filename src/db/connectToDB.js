311c2c90fcb5177fdeee08331a5c5f58ab06f298
StefanoBichicchi <bichicchi.stefano@gmail.com>
2020-11-28 12:11:27 +0100

    db fix

--- src/db/connectToDB.js
+++ src/db/connectToDB.js
@@ -1,24 +1,23 @@
 const mongoose = require('mongoose')
-const db = mongoose.connection
+const connection = mongoose.connection
 
 module.exports = function () {
-  if (db.readyState === 0) {
-    db.on('error', console.error.bind(console, 'Errore di connessione:'))
-    db.once('open', () => console.info('DB connesso correttamente'))
+  const db = global.db ? global.db : connection
+  db.on('error', console.error.bind(console, 'Errore di connessione:'))
+  db.once('open', () => console.info('DB connesso correttamente'))
 
-    let connectionUrl
+  let connectionUrl
 
-    if (process.env.NODE_ENV === 'production')
-      connectionUrl = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}${process.env.MONGO_URL}/${process.env.DB_NAME}?retryWrites=true`
-    else
-      connectionUrl = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_IP_DEV}:${process.env.DB_PORT}/${process.env.DB_NAME_TEST}`
+  if (process.env.NODE_ENV === 'production')
+    connectionUrl = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}${process.env.MONGO_URL}/${process.env.DB_NAME}?retryWrites=true`
+  else
+    connectionUrl = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_IP_DEV}:${process.env.DB_PORT}/${process.env.DB_NAME_TEST}`
 
-    mongoose.connect(connectionUrl, {
-      useNewUrlParser: true,
-      useCreateIndex: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false,
-      authSource: process.env.DB_AUTH,
-    })
-  }
+  mongoose.connect(connectionUrl, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    authSource: process.env.DB_AUTH,
+  })
 }
